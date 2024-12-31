c7f66b581f095cdabdc678b744b307dfbeedc323
Vineesh <vineesh@tartlabs.com>
2024-12-31 22:43:37 +0530

    Added build_runner command

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 	"github.com/vineeshvk/cleancli/dirvalid"
 	"github.com/vineeshvk/cleancli/input"
 	"github.com/vineeshvk/cleancli/models"
+	"github.com/vineeshvk/cleancli/utils"
 	"github.com/vineeshvk/cleancli/write"
 )
 
 func main() {
 	var mainDirectoryModel models.MainDirectoryModel = dirvalid.ValidateRootDirectories()
 
-	println(mainDirectoryModel.DataDir, mainDirectoryModel.DomainDir)
 	var apiInfo models.ApiInfoModel = input.GetAPIInfos(mainDirectoryModel)
 
 	write.WriteApiService(mainDirectoryModel.GetApiServiceRoute(), apiInfo)
@@ -23,5 +23,7 @@ func main() {
 	write.WriteDI(mainDirectoryModel, apiInfo)
 
 	fmt.Println(constants.CompletedIcon, "Completed adding API.")
+
+	utils.ExecuteBuildRunner(mainDirectoryModel.DataDir)
 
 }
